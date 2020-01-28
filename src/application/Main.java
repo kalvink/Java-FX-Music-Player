fb27faa32af051b246e3396d5b41dab4ef9c62e7
Kalvin <kalvin.kao@hotmail.com>
2020-01-28 10:43:53 -0500

    update

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,44 +1,36 @@
+
 package application;
 
-import java.awt.event.ActionEvent;
+import java.io.File;
 
 import javafx.application.Application;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 
+
 public class Main extends Application {
+	public static Window publicStage;
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			VBox root = (VBox) FXMLLoader.load(getClass().getResource("asd.fxml"));
+			VBox root = (VBox)FXMLLoader.load(getClass().getResource("Sample.fxml"));
 			Scene scene = new Scene(root, 350, 214);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.setResizable(false);
-			primaryStage.show();
-		} catch (Exception e) {
+			Stage publicStage = primaryStage;
+			publicStage.setScene(scene);
+			publicStage.setResizable(false);
+			publicStage.show();
+
+		} catch(Exception e) {
 			e.printStackTrace();
 		}
-
 	}
 
-	@FXML
-	public void openSoundFile(ActionEvent e) {
-		System.out.println("Menu clicked");
-	}
-	@FXML
-	public void exitApp(ActionEvent e) {
-		System.out.println("Menu clicked");
-	}
-	@FXML
-	public void openAbout(ActionEvent e) {
-		System.out.println("Menu clicked");
-	}
-	
 
 	public static void main(String[] args) {
 		launch(args);
--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -1,5 +1,69 @@
 package application;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import javafx.event.ActionEvent;
+
+import javafx.fxml.FXML;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.FileChooser;
+
 public class SampleController {
-	
+
+	@FXML
+	public void openFile(ActionEvent e) {
+
+		try {
+			FileChooser fileChooser = new FileChooser();
+			FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Audio files", "*.mp3", "*.wav",
+					"*.aac");
+			fileChooser.getExtensionFilters().add(extFilter);
+			fileChooser.setTitle("Open Audio File");
+			File file = fileChooser.showOpenDialog(Main.publicStage);
+			System.out.println(file);
+			String fpath = file.toString();
+			System.out.println(fpath);
+		} catch (Exception ex) {
+			System.out.println("Something went wrong.");
+			// playSong(fpath);
+		} finally {
+			System.out.println("The 'try catch' is finished.");
+		}
+	}
+
+	public void playSong(String fpath) {
+
+		Media media = new Media(fpath);
+		MediaPlayer mediaPlayer = new MediaPlayer(media);
+		mediaPlayer.setAutoPlay(true);
+
+	}
+
+	@FXML
+	public void exitApp(ActionEvent e) {
+		System.out.println("Menu clicked");
+	}
+
+	@FXML
+	public void openAbout(ActionEvent e) {
+		System.out.println("Menu clicked");
+	}
+
+	@FXML
+	public void prevTrack(ActionEvent e) {
+	}
+
+	@FXML
+	public void nextTrack(ActionEvent e) {
+	}
+
+	@FXML
+	public void playPause(ActionEvent e) {
+
+	}
+
 }
