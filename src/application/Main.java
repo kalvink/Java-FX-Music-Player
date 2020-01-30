eb4f1c8455c4ed52c9a831a6a43603ae9545c920
Kalvin <kalvin.kao@hotmail.com>
2020-01-30 10:17:23 -0500

    logo and css styles

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.stage.Window;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.VBox;
 import javafx.fxml.FXMLLoader;
 
@@ -21,7 +22,9 @@ public class Main extends Application {
 			publicStage.setScene(scene);
 			publicStage.setResizable(false);
 			publicStage.setTitle("Music Player by Kalvin Kao");
+			publicStage.getIcons().add(new Image("logo.png"));
 			publicStage.show();
+
 
 		} catch (Exception e) {
 			e.printStackTrace();
